3f12eddd9a92efb16c14f859ddf538c5a77cb785
fermarinsanchez <fermarinsanchez@yahoo.es>
2022-07-07 21:34:08 +0200

    Change News Order

--- json_projects/news.js
+++ json_projects/news.js
@@ -1,10 +1,10 @@
 export const news = [
-    {   
+    {
         "id": 0,
-        "title": "El georradar ofrece un corte del terreno cada 5 centímetros",
-        "font": "La Tribuna de Toledo",
-        "text": "Se trata de uno de los análisis más exhaustivos realizados en Vega Baja, cuyos responsables afirman que no tiene nada que ver con el georradar de 2009",
-        "link": "https://www.latribunadetoledo.es/noticia/Z40D2D7DB-CFCB-50A9-B1E9D74ADCA9B51A/202012/El-georradar-ofrece-un-corte-del-terreno-cada-5-centimetros"
+        "title": "Preserving a Roman ‘Billionaire’s Villa’ with 3D laser scanning",
+        "font": "Leica Geosystems",
+        "text": "Capturing pieces of history: multiple phases of excavation at Villa Noheda using a Leica RTC360 3D laser scanner for fast, precise data capture",
+        "link": "https://leica-geosystems.com/es-es/case-studies/reality-capture/preserving-a-roman-billionaires-villa-with-3d-laser-scanning"
     },
     {
         "id": 1,
@@ -27,11 +27,11 @@ export const news = [
         "text": "La Fundación, en colaboración con la Junta, la empresa Abscisa3d e Irica está digitalizando en tres dimensiones toda la fortaleza para conservar la información de las últimas campañas",
         "link": "https://www.latribunadeciudadreal.es/Noticia/ZCC739593-BB45-85A3-507B6239295872C5/202110/El-3d-llega-al-Castillo-de-la-Estrella-de-Montiel?fbclid=IwAR3Xb4KN58ltZY5FN1EZTr6CziZ1Vyjkbt-kqo8wbWTqVee8w5Xowr8Nl5E"
     },
-    {
+    {   
         "id": 4,
-        "title": "Preserving a Roman ‘Billionaire’s Villa’ with 3D laser scanning",
-        "font": "Leica Geosystems",
-        "text": "Capturing pieces of history: multiple phases of excavation at Villa Noheda using a Leica RTC360 3D laser scanner for fast, precise data capture",
-        "link": "https://leica-geosystems.com/es-es/case-studies/reality-capture/preserving-a-roman-billionaires-villa-with-3d-laser-scanning"
+        "title": "El georradar ofrece un corte del terreno cada 5 centímetros",
+        "font": "La Tribuna de Toledo",
+        "text": "Se trata de uno de los análisis más exhaustivos realizados en Vega Baja, cuyos responsables afirman que no tiene nada que ver con el georradar de 2009",
+        "link": "https://www.latribunadetoledo.es/noticia/Z40D2D7DB-CFCB-50A9-B1E9D74ADCA9B51A/202012/El-georradar-ofrece-un-corte-del-terreno-cada-5-centimetros"
     },
 ]
